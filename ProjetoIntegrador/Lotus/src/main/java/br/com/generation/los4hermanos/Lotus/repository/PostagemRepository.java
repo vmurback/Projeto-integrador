93446ff0d99b6de6272f2a19f9734675d07057d3
vmurback <83590663+vmurback@users.noreply.github.com>
2021-06-15 14:10:26 -0300

    Delete ProjetoIntegrador directory

--- ProjetoIntegrador/Lotus/src/main/java/br/com/generation/los4hermanos/Lotus/repository/PostagemRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.generation.los4hermanos.saltoalto.repository;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.generation.los4hermanos.saltoalto.model.Postagem;
-
-public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-    public List<Postagem> findAllByTituloContainingIgnoreCase(String postagem);
-    
-}
